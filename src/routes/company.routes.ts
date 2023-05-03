88fc8eb9f107da057d7e5397e48b0cccf96975a5
abdul.salam.shaikh <abd.salam.shaikh@gmail.com>
2023-05-04 00:34:34 +0200

    (Dev-Salam) Adding comments to the functions

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -1,99 +1,113 @@
-import { Request, Response, Router } from "express";
-import { v4 as uuidv4 } from "uuid";
+import { Request, Response, Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
+/**
+ * @Author Abdul Salam Shaikh
+ */
 const router = Router();
-const minCompanyNameLength = 2
-const maxCompanyNameLength = 60
-const minPhoneNumberLength = 2
-const maxPhoneNumberLength = 20
-const minNameLength = 2
-const maxNameLength = 60
-const validZipCodeLength = 5
+const minCompanyNameLength = 2;
+const maxCompanyNameLength = 60;
+const minPhoneNumberLength = 2;
+const maxPhoneNumberLength = 20;
+const minNameLength = 2;
+const maxNameLength = 60;
+const validZipCodeLength = 5;
 
 interface companyData {
   name: string;
-  phone: number
+  phone: number;
 }
 
 interface customerData {
   firstName: string;
   lastName: string;
   zipCode: number;
-  mail: string
+  mail: string;
 }
 
 interface companyDataParent {
-  id : string;
+  id: string;
   companyData: companyData;
-  customerData: customerData
+  customerData: customerData;
 }
 
 
-const companyDataPersistenceObject : companyDataParent[]= []
+const companyDataPersistenceObject: companyDataParent[] = [];
 
-router.post("/checkout", (req: Request & { body: companyDataParent }, res: Response) => {
+/**
+ * Post Method to populate company data and an additional id
+ * @Accepts Company Data
+ * @Returns Company Data with populated UUID
+ */
+router.post('/checkout', (req: Request & { body: companyDataParent }, res: Response) => {
   let companyData: companyData = req.body.companyData;
 
-  if(companyData.name.length > maxCompanyNameLength && companyData.name.length < minCompanyNameLength)
-  {
-    return res.status(400).json({ error: "Company Name should have length between 2 to 60 characters" });
+  if (companyData.name.length > maxCompanyNameLength && companyData.name.length < minCompanyNameLength) {
+    return res.status(400).json({ error: 'Company Name should have length between 2 to 60 characters' });
   }
 
-  if (typeof companyData.phone !== "number") {
-    return res.status(400).json({ error: "PhoneNumber is not a number" });
+  if (typeof companyData.phone !== 'number') {
+    return res.status(400).json({ error: 'PhoneNumber is not a number' });
   }
 
   if (companyData.phone.toString().length < minPhoneNumberLength || companyData.phone.toString().length > maxPhoneNumberLength) {
-    return res.status(400).json({ error: "PhoneNumber should be between 2 until 20 digits" });
+    return res.status(400).json({ error: 'PhoneNumber should be between 2 until 20 digits' });
   }
 
+  //Generate random uuid
   req.body.id = uuidv4();
   companyDataPersistenceObject.push(req.body);
   return res.status(200).json(companyDataPersistenceObject);
-
 });
 
+/**
+ * Populates additional customer data to an existing company object
+ * @Accepts CompanyID
+ * @Returns Company Object with Customer Data
+ */
 
-router.put("/checkout/:id", (req: Request & { body: customerData }, res: Response) => {
+router.put('/checkout/:id', (req: Request & { body: customerData }, res: Response) => {
   const companyId = req.params.id;
   const customerData: customerData = req.body.customerData;
 
   const companyParentObject: companyDataParent | undefined = companyDataPersistenceObject.find(obj => obj.id === companyId);
 
   if (companyParentObject == undefined) {
-    return res.status(400).json({ error: "Incorrect Company Id" });
+    return res.status(400).json({ error: 'Incorrect Company Id' });
   }
 
-  if(customerData.firstName.length > maxNameLength && customerData.firstName.length < minNameLength)
-  {
-    return res.status(400).json({ error: "Customer First Name should have length between 2 to 60 characters" });
+  if (customerData.firstName.length > maxNameLength && customerData.firstName.length < minNameLength) {
+    return res.status(400).json({ error: 'Customer First Name should have length between 2 to 60 characters' });
   }
 
-  if(customerData.lastName.length > maxNameLength && customerData.lastName.length < minNameLength)
-  {
-    return res.status(400).json({ error: "Customer Last Name should have length between 2 to 60 characters" });
+  if (customerData.lastName.length > maxNameLength && customerData.lastName.length < minNameLength) {
+    return res.status(400).json({ error: 'Customer Last Name should have length between 2 to 60 characters' });
   }
-  if (typeof customerData.zipCode !== "number") {
-    return res.status(400).json({ error: "Zipcode is not a number" });
-  }
-
-  if (customerData.zipCode.toString().length != validZipCodeLength ) {
-    return res.status(400).json({ error: "Zipcode should be exactly 5 digits" });
+  if (typeof customerData.zipCode !== 'number') {
+    return res.status(400).json({ error: 'Zipcode is not a number' });
   }
 
-  companyParentObject.customerData = customerData
+  if (customerData.zipCode.toString().length != validZipCodeLength) {
+    return res.status(400).json({ error: 'Zipcode should be exactly 5 digits' });
+  }
+
+  companyParentObject.customerData = customerData;
   companyDataPersistenceObject.push(companyParentObject);
   return res.status(200).json(companyDataPersistenceObject);
 
 });
 
-router.get("/checkout/:id", (req: Request, res: Response) => {
+/**
+ * @Accepts UUID
+ * @Returns Company Object matching with the UUID and customer data
+ */
+router.get('/checkout/:id', (req: Request, res: Response) => {
   const companyId = req.params.id;
 
   const companyParentObject: companyDataParent | undefined = companyDataPersistenceObject.find(obj => obj.id === companyId);
 
   if (companyParentObject == undefined) {
-    return res.status(400).json({ error: "Incorrect Company Id" });
+    return res.status(400).json({ error: 'Incorrect Company Id' });
   }
 
   return res.status(200).json(companyParentObject);
